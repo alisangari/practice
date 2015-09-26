67dba578e0d15a4da84afd60a0389b4c0b10f006
leon <qingfy841@gmail.com>
2015-09-26 19:04:06 -0400

    NetworkAddr

--- src/utility/string/NetworkAddr.java
+++ src/utility/string/NetworkAddr.java
@@ -18,22 +18,33 @@ public class NetworkAddr {
 	    int value = mask;
 	    byte[] bytes = new byte[]{ 
 	            (byte)(value >>> 24), (byte)(value >> 16 & 0xff), (byte)(value >> 8 & 0xff), (byte)(value & 0xff) };
-		byte[] inveseByte = new byte[]{
-				(byte)(~bytes[0]), (byte)(~bytes[1]), (byte)(~bytes[2]), (byte)(~bytes[3])};
-
+		
 		try {
 			InetAddress netAddr = InetAddress.getByAddress(bytes);
 			System.out.println("Mask=" + netAddr.getHostAddress());
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		}
+		
+		byte[] inveseBytes = new byte[]{
+				(byte)(~bytes[0]), (byte)(~bytes[1]), (byte)(~bytes[2]), (byte)(~bytes[3])};
 	    
+		String[] ips = ip.split("\\.");
+				
+		int[] ipsInt = {Integer.parseInt(ips[0]), Integer.parseInt(ips[1]),Integer.parseInt(ips[2]),Integer.parseInt(ips[3])};
+		
+		int[] NetWork = {bytes[0] &ipsInt[0], bytes[1] &ipsInt[1],bytes[2] &ipsInt[2],bytes[3] &ipsInt[3] };
+		int[] Broadcast = {inveseBytes[0] | ipsInt[0], inveseBytes[1] | ipsInt[1],inveseBytes[2] | ipsInt[2], (inveseBytes[3] | ipsInt[3]) &0xff};
+		
+		
 	    System.out.println("Address=" + ip);
-	    System.out.println("Usable addresses NO="+(Math.pow(2, 32-prefix)-2));
+	    System.out.println("Usable addresses NO="+(int)(Math.pow(2, 32-prefix)-2));
+	    System.out.println("Network="+NetWork[0]+"."+NetWork[1]+"."+NetWork[2]+"."+NetWork[3]);
+	    System.out.println("Broadcast="+Broadcast[0]+"."+Broadcast[1]+"."+Broadcast[2]+"."+Broadcast[3]);
 	}
 	
 	public static void main(String argv[]){
-		FetchNetaddrFromCIDR("192.168.10.0/24");
+		FetchNetaddrFromCIDR("192.168.10.10/30");
 	}
 	
 }
